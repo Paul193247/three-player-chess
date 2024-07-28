8fca3651c34c32c46348f3bd7f488305361da0e6
Paul Zörb <paulzoerb09@icloud.com>
2024-07-28 13:28:09 +0200

    vercel.json

--- api/index.py
+++ api/index.py
@@ -12,4 +12,4 @@ def home():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return jsonify({"status": 404, "message": "Not Found"}), 404
+    return "Flask Vercel Example - Hello World", 404
